afa9135ded0a5f2d5bc03349a6fa46193507dc4d
Andy Mender <amender@localhost.localdomain>
2020-01-30 21:08:34 +0100

    Add extra attributes to `MainController` for testing

--- controllers/default.go
+++ controllers/default.go
@@ -4,12 +4,29 @@ import (
 	"github.com/astaxie/beego"
 )
 
+// MainController is a composition wrapper around `beego.Controller`
 type MainController struct {
 	beego.Controller
 }
 
+// Get method of `MainController` generates the primary HTTP response
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	// Base page layout
+	c.Layout = "template/layout.html"
+
+	// Fields loaded into the template
+	c.Data["Title"] = "Lands of Unix"
+	c.Data["EntryTitle"] = "Welcome!"
+	c.TplName = "index.html"
+
+	// Blog post listing
+	BlogEntries := make(map[string]string)
+	BlogEntries["Fishing with Ubuntu"] = "/"
+	BlogEntries["I Love nVidia"] = "/"
+	BlogEntries["A Very Long Blog Post Title Which I Don't Want. Does This Even Fit???"] = "/"
+	c.Data["BlogEntries"] = BlogEntries
+
+	// Additional dynamic layout sections
+	c.LayoutSections = make(map[string]string)
+
 }
