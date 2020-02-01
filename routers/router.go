22e93b94cd19217b3ab56fbca93c422b5adb3953
Andy Mender <amender@localhost.localdomain>
2020-02-01 18:53:57 +0100

    Define URL routes in `routers`

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,21 @@ package routers
 
 import (
 	"linuxmender/controllers"
+	"linuxmender/models"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	// Create central route controller object
+	ctrl := &controllers.RouteController{
+		EntryRecords: models.GetEntries(),
+	}
+
+	// Attach controller callback objects to URL paths
+	beego.Router("/", ctrl, "get:GetIndex")
+	// TODO: separate "index" route from regular entry routes
+	beego.Router("/index", ctrl, "get:GetIndex")
+	beego.Router("/notfound", ctrl, "get:GetNotFound")
+	beego.Router("/:entry", ctrl, "get:GetEntry")
 }
