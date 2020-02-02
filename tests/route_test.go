74953f5b40e9ead8138554b94203ceb3357c7292
Andy Mender <amender@localhost.localdomain>
2020-02-02 13:27:01 +0100

    Convert beego template test to route_test test suite

---
+++ tests/route_test.go
@@ -0,0 +1,55 @@
+package test
+
+import (
+	_ "linuxmender/routers"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/astaxie/beego"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func init() {
+	_, file, _, _ := runtime.Caller(0)
+	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator))))
+	beego.TestBeegoInit(apppath)
+}
+
+// TestRootEndpoint is a test for the main root "/" endpoint
+func TestRootEndpoint(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestRootEndpoint", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Root Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+	})
+}
+
+// TestEntryEndpoint is a test for the blog entry endpoints
+func TestEntryEndpoint(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/000", nil)
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	beego.Trace("testing", "TestEntryEndpoint", "Code[%d]\n%s", w.Code, w.Body.String())
+
+	Convey("Subject: Test Entry Endpoint\n", t, func() {
+		Convey("Status Code Should Be 200", func() {
+			So(w.Code, ShouldEqual, 200)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+	})
+}
