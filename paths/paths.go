464f295dd63a38a97b10dd9992c6c40ef1c646b0
Andy Mender <amender@localhost.localdomain>
2020-02-08 23:40:20 +0100

    Add a ViewsPath to the `paths` module

--- paths/paths.go
+++ paths/paths.go
@@ -8,5 +8,8 @@ var ProjectPath, _ = filepath.Abs(".")
 // StaticPath points to the "static" sub-dir
 var StaticPath = filepath.Join(ProjectPath, "static")
 
+// ViewsPath points to the "views" sub-dir
+var ViewsPath = filepath.Join(ProjectPath, "views")
+
 // EntriesPath points to the JSON containing blog entry definitions
 var EntriesPath = filepath.Join(StaticPath, "entries.json")
