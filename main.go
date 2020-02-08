db50a33c5f5b61a5c10bbd3480762712d198a0fe
Andy Mender <amender@localhost.localdomain>
2020-02-08 18:40:15 +0100

    Attach custom template functions to beego

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"linuxmender/routers"
 	_ "linuxmender/routers"
 
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	// Add custom functions for in-template manipulations
+	beego.AddFuncMap("nextEntry", routers.NextEntry)
+	beego.AddFuncMap("previousEntry", routers.PreviousEntry)
+	beego.AddFuncMap("isValidEntry", routers.IsValidEntry)
+
 	// Run Web server
 	beego.Run()
 }
