d3a71c49405e655f2e92da3c3cb753b8fcaf3530
AndyMender <andymenderunix@gmail.com>
2020-02-23 14:52:25 +0100

    Provide API to SQLite3 backend with entry records

--- models/entry.go
+++ models/entry.go
@@ -1,9 +1,12 @@
 package models
 
 import (
+	"database/sql"
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"strings"
 )
 
 // Entry is a definition for Blog entry objects
@@ -13,9 +16,136 @@ type Entry struct {
 	Tags              []string
 }
 
+// CreateEntriesTable creates an SQL table for storing blog entries
+func CreateEntriesTable(db *sql.DB) error {
+	sqlQuery := `
+		CREATE TABLE IF NOT EXISTS entries (
+      id INTEGER NOT NULL PRIMARY KEY,
+      title TEXT NOT NULL,
+			date_posted TEXT NOT NULL,
+			tags TEXT DEFAULT NULL
+		);
+	`
+
+	_, err := db.Exec(sqlQuery)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlQuery)
+		return err
+	}
+
+	return nil
+}
+
+// InsertEntryOne inserts a single Entry record into a SQL table
+func InsertEntryOne(entry *Entry, db *sql.DB) {
+	sqlQuery := "INSERT INTO entries (id, title, date_posted, tags) VALUES (?, ?, ?, ?)"
+
+	// Create a "prepared" SQL statement context
+	readyStatement, err := db.Prepare(sqlQuery)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer readyStatement.Close()
+
+	// Execute statement
+	_, err = readyStatement.Exec(entry.ID, entry.Title, entry.DatePosted, strings.Join(entry.Tags, ","))
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+// InsertEntryMany is analogous to InsertEntryOne, but accepts a slice of Entry records
+func InsertEntryMany(entries map[string]Entry, db *sql.DB) {
+	sqlQuery := "INSERT INTO entries (id, title, date_posted, tags) VALUES (?, ?, ?, ?)"
+
+	// Create a "prepared" SQL statement context
+	readyStatement, err := db.Prepare(sqlQuery)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer readyStatement.Close()
+
+	// Loop over entry records and insert them one by one
+	for _, entry := range entries {
+		_, err = readyStatement.Exec(entry.ID, entry.Title, entry.DatePosted, strings.Join(entry.Tags, ","))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+// GetEntryOne returns a single Entry record from a SQL table
+func GetEntryOne(entryID int, db *sql.DB) *Entry {
+	// Create a blank Entry record
+	entry := &Entry{}
+
+	// Create a "prepared" SQL statement context
+	sqlQuery := "SELECT id, title, date_posted, tags FROM entries WHERE id = ?"
+	readyStatement, err := db.Prepare(sqlQuery)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	defer readyStatement.Close()
+
+	// Fetch entry record
+	var tagsText string
+	err = readyStatement.QueryRow(entryID).Scan(&entry.ID, &entry.Title, &entry.DatePosted, &tagsText)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	// Convert tags to a slice
+	entry.Tags = strings.Split(tagsText, ",")
+
+	return entry
+}
+
+// GetEntriesAll returns all Entry records from a SQL table
+func GetEntriesAll(db *sql.DB) map[string]*Entry {
+	entryRecords := make(map[string]*Entry)
+
+	// Generate a Rows iterator from a SQL query
+	sqlQuery := "SELECT id, title, date_posted, tags FROM entries"
+	rows, err := db.Query(sqlQuery)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	defer rows.Close()
+
+	// Iterate over rows and populate entry records
+	for rows.Next() {
+		var (
+			entryID                     int
+			title, datePosted, tagsText string
+		)
+
+		err = rows.Scan(&entryID, &title, &datePosted, &tagsText)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		entryRecords[strconv.Itoa(entryID)] = &Entry{
+			ID:         entryID,
+			Title:      title,
+			DatePosted: datePosted,
+			Tags:       strings.Split(tagsText, ","),
+		}
+	}
+
+	return entryRecords
+}
+
 // GetEntries reads the entries JSON file and returns a slice of Entry records
-func GetEntries(entriesPath string) map[string]Entry {
-	entryRecords := make(map[string]Entry)
+func GetEntries(entriesPath string) map[string]*Entry {
+	entryRecords := make(map[string]*Entry)
 
 	entriesText, err := ioutil.ReadFile(entriesPath)
 	if err != nil {
