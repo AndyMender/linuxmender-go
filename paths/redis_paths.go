292c99581858c71234f0d4f7cffd7a3059e889c9
AndyMender <andymenderunix@gmail.com>
2020-03-01 18:36:23 +0100

    Split `paths` module across local and Redis paths

---
+++ paths/redis_paths.go
@@ -0,0 +1,10 @@
+package paths
+
+// ProjectRedisPath is the "root" of all Redis paths
+var ProjectRedisPath = "linuxmender"
+
+// SessionsRedisPath is the sub-path for user session keys
+var SessionsRedisPath = ProjectRedisPath + ":sessions"
+
+// PostsRedisPath is the sub-path for blog entry data
+var PostsRedisPath = ProjectRedisPath + ":posts"
